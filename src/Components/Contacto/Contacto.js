9697bba19e9ad2260a6c29cf41959c4934cbdfec
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-07-26 16:42:53 -0300

    contacto y footer terminados

--- src/Components/Contacto/Contacto.js
+++ src/Components/Contacto/Contacto.js
@@ -3,22 +3,22 @@ import './Contacto.css';
 
 const Contacto = () => {
   return (
-    <div className="bg-danger py-4">
-          <h4 className="text-light pb-3">Contacto</h4>
+    <div className="py-4">
+          <h2 className="title-contacto text-light pb-3">Contacto</h2>
       <div className="area-contacto d-flex">
-      <div className="px-4 col-6">
-      <iframe title="mapas" src="https://www.google.com/maps/d/embed?mid=1Fh5MQu9cVwJvKqGwBzNVdepmaVPAqGA&ehbc=2E312F" width="500" height="350" className="mx-2 mapa"></iframe>
+      <div className="px-4 col-6 ">
+      <iframe title="mapas" src="https://www.google.com/maps/d/embed?mid=1Fh5MQu9cVwJvKqGwBzNVdepmaVPAqGA&ehbc=2E312F" width="500" height="400" className="mx-2 mapa rounded-3"></iframe>
     </div>
 
 <div className="div-email col-6">
     <form className="area-contacto-email">
-      <label className="pb-1 text-light fs-5">Nombre</label><br />
-      <input type="text" className="input-nombre w-50" /> <br />
-      <label className="pb-1 text-light fs-5">Email de contacto</label><br />
-      <input type="email" className="w-50 input-email" /> <br />
-      <label className="pb-1 text-light fs-5">Mensaje</label> <br />
-      <textarea name="mensaje" id="mensaje" cols="30" rows="5" class="cuadro-mensaje p-1 w-75" required></textarea> <br />
-      <input type="button" className="btn btn-light w-25 mt-2" value="Enviar" />
+      <label className="pb-1 text-light fs-4">Nombre/Asunto</label><br />
+      <input type="text" className="input-nombre fs-5 p-2 rounded-3 border-0 w-50" /> <br />
+      <label className="pb-1 text-light fs-4 pt-2">Email de contacto</label><br />
+      <input type="email" className="w-50 fs-5 input-email p-2 rounded-3 border-0" placeholder="nombre@email.com" /> <br />
+      <label className="pb-1 text-light fs-4 pt-2">Mensaje</label> <br />
+      <textarea name="mensaje" id="mensaje" cols="30" rows="5" class="cuadro-mensaje rounded-3 border-0 p-2 w-75" required></textarea> <br />
+      <input type="button" className="btn-enviar-contacto btn w-25 mt-2" value="Enviar" />
     </form>
     </div>
     </div>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      <h4 className="p-3">Todos los derechos reservados a La Quiaqueña Drugstores | 2022</h4>
+    <div className="footer">
+      <h6 className="p-5 mb-0 text-light">Todos los derechos reservados a La Quiaqueña Drugstores | 2022</h6>
     </div>
   );
 };
