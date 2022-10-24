ec09b9554b1d0282b93d3986639484cff8d5a30d
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-24 18:37:48 -0300

    deploy

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className="footer">
       <h5 className="pt-4 pb-2 text-light">Todos los derechos reservados a La Quiaqueña Drugstores | 2022</h5>
-      <a href="/login">
+      <Link to="/login">
       <p className="pb-4 mb-0 text-light"><i>Logueo del Administrador</i></p>
-      </a>
+      </Link>
     </div>
   );
 };
