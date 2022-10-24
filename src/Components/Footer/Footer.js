d738c8177259e256a0573a030d490fe5b34d8c17
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-24 11:28:31 -0300

    run build y link listos

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <div className="footer">
       <h5 className="pt-4 pb-2 text-light">Todos los derechos reservados a La Quiaqueña Drugstores | 2022</h5>
-      <a href="/login">
+      <Link href="/login">
       <p className="pb-4 mb-0 text-light"><i>Logueo del Administrador</i></p>
-      </a>
+      </Link>
     </div>
   );
 };
