ee2db1b012993a8dd5915e16dc893ab09f6e5d2c
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-31 14:10:39 -0300

    interfaz lista

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -4,23 +4,10 @@ import hojas from '../assets/images/logo-carousel.png';
 
 const Carousel = () => {
   return (
-      <div>
+      <div className="carousel-height">
         <div className="logo-carousel w-100 d-flex justify-content-center align-items-center">
          <img src={hojas} className="hojas-carousel w-75" alt="" srcset="" />
         </div>
-    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-height carousel-inner">
-    <div className="carousel-item active">
-      <img src="https://images.pexels.com/photos/39828/paper-colorful-color-loose-39828.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src="https://images.pexels.com/photos/39828/paper-colorful-color-loose-39828.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src="https://images.pexels.com/photos/39828/paper-colorful-color-loose-39828.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..." />
-    </div>
-  </div>
-</div>
 </div>
   );
 };
--- src/Pages/Productos/Productos.js
+++ src/Pages/Productos/Productos.js
@@ -28,7 +28,7 @@ const Productos = () => {
       <Navbar />
     <div className="productos-page d-flex mt-5">
 
-<div className="div-productos-page d-flex flex-wrap align-items-center justify-content-center mt-5">
+<div className="div-productos-page d-flex flex-wrap justify-content-center mt-5">
 { 
    productos.map(producto => (
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2 border-0 rounded-3" key={producto._id}>
@@ -36,7 +36,7 @@ const Productos = () => {
       <img src={producto.image.secure_url} className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">{producto.nombre}</h5>
-        <p className="card-text py-2 m-0">{producto.precio}</p>
+        <p className="card-text py-2 m-0">${producto.precio}</p>
         <a href={`https://api.whatsapp.com/send?phone=5493812183467&text=%C2%A1Hola%20*La%20Quiaque%C3%B1a%20Herborister%C3%ADa*%20%F0%9F%91%8B%F0%9F%8C%BF%20!%20Visit%C3%A9%20su%20p%C3%A1gina%20web%20y%20quisiera%20informaci%C3%B3n%20del%20producto%20*${producto.nombre}*.%20Muchas%20gracias!%20%E2%98%BA`} target="new" >
         <button type="button" className="boton-productos-destacados btn text-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Comprar</button></a>
       </div>
