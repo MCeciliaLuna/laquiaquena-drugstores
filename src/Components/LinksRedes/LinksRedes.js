0584ec466e2492f2b90e16fd5629d33b81ef624d
MCeciliaLuna <lunama.cecilia@gmail.com>
2023-03-29 20:18:10 -0300

    link a whatsapp de home agregado

--- src/Components/LinksRedes/LinksRedes.js
+++ src/Components/LinksRedes/LinksRedes.js
@@ -4,7 +4,7 @@ import './LinksRedes.css'
 const LinksRedes = () => {
   return (
       <>
-      <a target="new" href="https://www.whatsapp.com/?lang=es" className="link-redes fs-3 d-flex align-items-center justify-content-center py-4"><ion-icon name="logo-whatsapp"></ion-icon>Whatsapp</a>
+      <a target="new" href="https://wa.me/5493816613960" className="link-redes fs-3 d-flex align-items-center justify-content-center py-4"><ion-icon name="logo-whatsapp"></ion-icon>Whatsapp</a>
 </>
   );
 };
